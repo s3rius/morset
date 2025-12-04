26bd6fa7dbb1f3a177b451c58ca0fd07cfee8d9a
Pavel Kirilin <win10@list.ru>
2025-12-04 01:53:21 +0100

    Added audio playing.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use crossterm::event::{
 };
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use crossterm::{self, execute};
+use rodio::source::SineWave;
 use std::io::{Write, stdout};
 use std::time::Duration;
 
@@ -12,10 +13,24 @@ struct Args {
     /// Words per minute
     #[clap(short, long, default_value_t = 10)]
     wpm: u64,
+    /// Silent mode (no sound)
+    #[clap(short, long, default_value_t = false)]
+    silent: bool,
+    // Volume in percentage 1-100
+    #[clap(short, long, default_value_t = 11)]
+    volume: u8,
 }
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
+
+    let stream = rodio::OutputStreamBuilder::open_default_stream().ok();
+    let mut sink = stream.as_ref().map(|s| rodio::Sink::connect_new(s.mixer()));
+    if args.silent {
+        sink.take();
+    }
+    sink.as_ref()
+        .map(|s| s.set_volume(args.volume as f32 / 100.0));
 
     enable_raw_mode()?;
 
@@ -62,6 +77,14 @@ fn main() -> anyhow::Result<()> {
         ('9', "----."),
         ('0', "-----"),
         ('.', ".-.-.-"),
+        ('!', "-.-.--"),
+        ('\'', ".----."),
+        (',', "--..--"),
+        ('?', "..--.."),
+        ('/', "-..-."),
+        ('-', "-....-"),
+        ('(', "-.--.-"),
+        (')', "-.--."),
     ];
     let wpm = args.wpm as f64;
 
@@ -98,12 +121,18 @@ fn main() -> anyhow::Result<()> {
                             crossterm::terminal::Clear(crossterm::terminal::ClearType::CurrentLine),
                             crossterm::cursor::MoveToColumn(0)
                         )?;
+                        continue;
                     }
                     if kev.is_press() {
+                        if holding {
+                            continue;
+                        }
                         holding = true;
+                        sink.as_ref().map(|s| s.append(SineWave::new(600.0)));
                         last_press = std::time::Instant::now();
                     }
                     if kev.is_release() {
+                        sink.as_ref().map(|s| s.stop());
                         holding = false;
                         let now = std::time::Instant::now();
                         let passed = now.duration_since(last_press);
@@ -122,17 +151,14 @@ fn main() -> anyhow::Result<()> {
             }
             empties += 1;
             if empties == 3 {
+                let buffer_chrs = buffer
+                    .iter()
+                    .map(|&b| if b { '.' } else { '-' })
+                    .collect::<String>();
+
                 for &(ch, code) in &abc {
-                    if code.len() == buffer.len() {
-                        let mut matched = true;
-                        for (i, c) in code.chars().enumerate() {
-                            let is_dit = c == '.';
-                            if buffer[i] != is_dit {
-                                matched = false;
-                                break;
-                            }
-                        }
-                        if matched {
+                    if code.len() == buffer_chrs.len() {
+                        if code == buffer_chrs {
                             text.push(ch);
                             print!("{}", ch);
                             stdout().flush()?;
