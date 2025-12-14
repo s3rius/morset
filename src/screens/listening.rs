b96b7ca3312fbc6ee1d74a2431a67314e9ab43dd
Pavel Kirilin <win10@list.ru>
2025-12-14 02:43:30 +0100

    Fixed smth, idk.

--- src/screens/listening.rs
+++ src/screens/listening.rs
@@ -1,28 +1,44 @@
 use bevy::prelude::*;
-use bevy_egui::{EguiContexts, egui};
+use bevy_egui::{EguiContexts, EguiPrimaryContextPass, egui};
 
 use crate::state::AppState;
 
 pub struct ListeningScreenPlugin;
 
 impl Plugin for ListeningScreenPlugin {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.add_systems(
+            EguiPrimaryContextPass,
+            setup_egui_main_menu.run_if(in_state(AppState::Listening)),
+        )
+        .add_systems(OnEnter(AppState::Listening), init_screen);
+    }
 }
 
+#[derive(Resource, Debug)]
 struct ListeningState {
-    pub wpm: u8,
-    pub frequency: usize,
-    pub volume: usize,
+    wpm: u8,
+    frequency: usize,
+    volume: usize,
 }
 
 fn setup_egui_main_menu(
     mut contexts: EguiContexts,
-    mut state: ResMut<NextState<AppState>>,
-    mut exit_writer: MessageWriter<AppExit>,
+    _state: ResMut<NextState<AppState>>,
+    _exit_writer: MessageWriter<AppExit>,
 ) -> Result {
     let ctx = contexts.ctx_mut()?;
     egui::CentralPanel::default().show(ctx, |ui| {
-        ui.vertical_centered_justified(|ui| {});
+        ui.vertical_centered_justified(|_ui| {});
     });
     Ok(())
+}
+
+fn init_screen(mut cmds: Commands) {
+    let state = ListeningState {
+        wpm: 10,
+        frequency: 550,
+        volume: 50,
+    };
+    cmds.insert_resource(state);
 }
